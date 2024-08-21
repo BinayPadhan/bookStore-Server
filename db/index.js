8fa640a902ab08b1b9d80acccff272bf4ded4328
BinayPadhan <binaypadhan26@gmail.com>
2024-08-21 19:45:44 +0530

    db updated!!

--- db/index.js
+++ db/index.js
@@ -3,10 +3,13 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        // Separate the base URI and query parameters
+        const [baseUri, queryParams] = process.env.MONGODB_URI.split("?");
+        // Reconstruct the URI with the database name in the correct position
+        const connectionString = `${baseUri}/${DB_NAME}?${queryParams}`;
+
+        const connectionInstance = await mongoose.connect(connectionString);
+
         console.log(`\n MongoDB connected !!! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("ERROR", error);
