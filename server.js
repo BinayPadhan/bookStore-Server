c8cef6a2a741d9c26a4f7576078f5755057c6cad
BinayPadhan <binaypadhan26@gmail.com>
2025-05-10 20:21:46 +0530

    update cors

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ import cors from "cors";
 const app = express()
 
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000', 
+  credentials: true
+}));
+
 app.use(express.json());
 
 import bookRouter from "./routes/book.routes.js";
